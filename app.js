8963cfb55a0f3cde73ef16b25ef5a852afffeae6
kode-knut <husby.jannicke@gmail.com>
2026-04-24 15:21:40 +0200

    Update path to service-worker

--- app.js
+++ app.js
@@ -402,7 +402,7 @@ function createConfetti() {
 // PWA installering
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./service-worker.js')
+        navigator.serviceWorker.register('service-worker.js')
             .then(registration => {
                 console.log('Service Worker registrert:', registration);
             })
